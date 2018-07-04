5199e6c063d6c22776db202e4a4e7292b982f10f
Maverick Tesini <mavericktesini@gmail.com>
2018-07-04 09:16:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,10 @@ class Application
 
     current_hour = Time.now.hour
 
-    if current_hour < 12
-      resp.write "Good Morning!"
-    else
+    if current_hour > 12
       resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
     end
 
     resp.finish
